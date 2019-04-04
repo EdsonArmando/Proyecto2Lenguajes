13ce1cd21260bdf4cb66bf750cdc98165d50fc86
LumationDevs <Eguix-2010302@kinal.edu.gt>
2019-04-04 15:37:08 -0600

    Agregando pdf de lista de tokens

--- Proyecto2/Form1.Designer.cs
+++ Proyecto2/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idTexto = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.analizarToolStripMenuItem,
+            this.acercaDeToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.guardarComoToolStripMenuItem.Text = "GuardarComo";
+            // 
+            // analizarToolStripMenuItem
+            // 
+            this.analizarToolStripMenuItem.Name = "analizarToolStripMenuItem";
+            this.analizarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.analizarToolStripMenuItem.Text = "Analizar";
+            this.analizarToolStripMenuItem.Click += new System.EventHandler(this.analizarToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.acercaDeToolStripMenuItem.Text = "AcercaDe";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // idTexto
+            // 
+            this.idTexto.Location = new System.Drawing.Point(12, 27);
+            this.idTexto.Name = "idTexto";
+            this.idTexto.Size = new System.Drawing.Size(584, 411);
+            this.idTexto.TabIndex = 1;
+            this.idTexto.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.idTexto);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Proyecto2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox idTexto;
     }
 }
 
--- Proyecto2/Form1.cs
+++ Proyecto2/Form1.cs
@@ -1,20 +1,203 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Font = iTextSharp.text.Font;
 
 namespace Proyecto2
 {
     public partial class Form1 : Form
     {
+        private string textAnalizar;
+        private ArrayList listaToken = new ArrayList();
+        private int no = 1;
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void analizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            analizadorLexico();
+            generarPDF();
+        }
+        private void analizadorLexico() {
+            textAnalizar = idTexto.Text;
+            int estadoInicial = 0;
+            int fila = 1;
+            int columna = 1;
+            string token = "";
+            char cadena;
+            int estado = 0;
+            for (estadoInicial = 0; estadoInicial < textAnalizar.Length; estadoInicial++)
+            {
+                cadena = textAnalizar[estadoInicial];
+                switch (estado)
+                {
+                    case 0:
+                        switch (cadena)
+                        {
+                            case ' ':
+                            case '\r':
+                            case '\t':
+                            case '\f':
+                                estado = 0;
+                                break;
+                            case '\n':
+                                fila++;
+                                columna = 1;
+                                estado = 0;
+                                break;
+                            case '{':
+                                token += cadena;
+                                estado = 1;
+                                estadoInicial = estadoInicial - 1;
+                                break;
+                            case '}':
+                                token += cadena;
+                                estado = 1;
+                                estadoInicial = estadoInicial - 1;
+                                break;
+                            default:
+                                columna++;
+                                break;
+                        }
+                        break;
+                    /*Estado de aceptacion */
+                    case 1:
+                        if (token.Equals("{")) {
+                            verificarToken(token, fila, columna, "CORCHETE APERTURA", estadoInicial);
+                            columna++;
+                            token = "";
+                            estado = 0;
+                        } else if (token.Equals("}")) {
+                            verificarToken(token, fila, columna, "CORCHETE CIERRE", estadoInicial);
+                            columna++;
+                            token = "";
+                            estado = 0;
+                        }
+                        break;
+                }
+
+            }
+        }
+        private void verificarToken(string token, int fila, int columna, string tipo, int posicion)
+        {
+            if (tipo.Equals("CORCHETE APERTURA")) {
+                listaToken.Add(new Token(no, 10, token, tipo, fila, columna, posicion));
+                no++;
+            } else if (tipo.Equals("CORCHETE CIERRE")) {
+                listaToken.Add(new Token(no, 20, token, tipo, fila, columna, posicion));
+                no++;
+            }
+        }
+        private void generarPDF() {
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc,
+                            new FileStream(Path.Combine(@"c:\Proyecto1\", "TablaSimbolos.pdf"), FileMode.Create));
+            doc.Open();
+            Paragraph titulo = new Paragraph("TABLA SIMBOLOS");
+            titulo.Alignment = Element.ALIGN_CENTER;
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(@"c:\Proyecto1\logo.png");
+           
+            logo.Alignment = 6;
+            doc.Add(logo);
+            doc.Add(new Paragraph("Universidad de San Carlos de Guatemala"));
+            doc.Add(new Paragraph("Facultad de Ingeniería"));
+            doc.Add(new Paragraph("Escuela de Ciencias y Sistemas"));
+            doc.Add(new Paragraph("Lenguajes Formales y de Programación"));
+            doc.Add(new Paragraph("Catedrática: Inga Damaris Campos"));
+            doc.Add(new Paragraph("Aux.: Aylin Aroche"));
+            doc.Add(new Paragraph("Sección:  A-"));
+            doc.Add(new Paragraph("\n"));
+            doc.Add(new Paragraph(titulo));
+            doc.Add(Chunk.NEWLINE);
+            /*Tabla tokens*/
+
+
+            // Creamos una tabla que contendrá el nombre, apellido y país
+            // de nuestros visitante.
+            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12, iTextSharp.text.Font.BOLD, BaseColor.BLACK);
+            PdfPTable tblPrueba = new PdfPTable(6);
+            tblPrueba.WidthPercentage = 100;
+
+            // Configuramos el título de las columnas de la tabla
+            PdfPCell no = new PdfPCell(new Phrase("No", _standardFont));
+            no.BorderWidth = 0;
+            no.BorderWidthBottom = 0.40f;
+
+            PdfPCell idNo = new PdfPCell(new Phrase("IdToken", _standardFont));
+            idNo.BorderWidth = 0;
+            idNo.BorderWidthBottom = 0.40f;
+
+            PdfPCell lexema = new PdfPCell(new Phrase("Lexema", _standardFont));
+            lexema.BorderWidth = 0;
+            lexema.BorderWidthBottom = 0.40f;
+
+            PdfPCell tipo = new PdfPCell(new Phrase("Tipo", _standardFont));
+            tipo.BorderWidth = 0;
+            tipo.BorderWidthBottom = 0.40f;
+
+            PdfPCell fila = new PdfPCell(new Phrase("Fila", _standardFont));
+            fila.BorderWidth = 0;
+            fila.BorderWidthBottom = 0.40f;
+
+            PdfPCell columna = new PdfPCell(new Phrase("Columna", _standardFont));
+            columna.BorderWidth = 0;
+            columna.BorderWidthBottom = 0.40f;
+
+            // Añadimos las celdas a la tabla
+            tblPrueba.AddCell(no);
+            tblPrueba.AddCell(idNo);
+            tblPrueba.AddCell(lexema);
+            tblPrueba.AddCell(tipo);
+            tblPrueba.AddCell(fila);
+            tblPrueba.AddCell(columna);
+
+            // Llenamos la tabla con información
+            foreach (Token t in listaToken) {
+                no = new PdfPCell(new Phrase(Convert.ToString(t.No), _standardFont));
+                no.BorderWidth = 0;
+
+                idNo = new PdfPCell(new Phrase(Convert.ToString(t.Tokens), _standardFont));
+                idNo.BorderWidth = 0;
+
+                lexema = new PdfPCell(new Phrase(t.Lexema, _standardFont));
+                lexema.BorderWidth = 0;
+
+                tipo = new PdfPCell(new Phrase(t.Tipo, _standardFont));
+                tipo.BorderWidth = 0;
+
+                fila = new PdfPCell(new Phrase(Convert.ToString(t.Fila), _standardFont));
+                fila.BorderWidth = 0;
+
+                columna = new PdfPCell(new Phrase(Convert.ToString(t.Columna), _standardFont));
+                columna.BorderWidth = 0;
+
+                // Añadimos las celdas a la tabla
+                tblPrueba.AddCell(no);
+                tblPrueba.AddCell(idNo);
+                tblPrueba.AddCell(lexema);
+                tblPrueba.AddCell(tipo);
+                tblPrueba.AddCell(fila);
+                tblPrueba.AddCell(columna);
+
+            }
+            doc.Add(tblPrueba);
+            doc.Close();
+            writer.Close();
+            Process.Start(@"c:\Proyecto1\TablaSimbolos.pdf");
         }
     }
 }
